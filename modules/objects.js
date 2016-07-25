1f06b24ef754a392fcdf12a7122b38c01313ad3f
sqiggg <github@sqiggg.com>
2016-07-25 15:10:07 -0700

    merged

--- modules/objects.js
+++ modules/objects.js
@@ -1,8 +1,41 @@
-function setup(){
-	createCanvas(500, 300);
-	background(255);
+var shack = function() {
+	this.fansPerClick;
+
+	this.onClick = function(fans) {
+		return fans += this.fansPerClick;
+	}
+}
+var Building = function(){
+	this.baseFps;
+	this.initCost;
+	this.description;
+	this.name;
+	this.amount;
+	this.modifier;
+	this.totalFansMade;
+	this.cost;
+
+	this.getFps = function(){
+		return this.baseFps * this.modifier * this.amount;
+	}
+	this.applyModifier = function(mod){
+		this.modifier += mod;
+	}
+	this.buy = function(amount){
+		this.amount += amount
+	}
 }
 
-function draw(){
-}
+var Upgrade = function(){
+	this.modifier;
+	this.name;
+	this.quote;
+	this.description;
+	this.affectedBuilding;
+	this.bought = false;
 
+	this.buy = function(){
+		this.bought = true;
+		Building.[this.affectedBuilding].applyModifier(this.modifier);
+	}
+}
