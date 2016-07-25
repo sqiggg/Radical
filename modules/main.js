85f778a42a090f1c75a2b3787bb3880d3d28e149
sqiggg <github@sqiggg.com>
2016-07-25 15:35:22 -0700

    worked on main.js

--- modules/main.js
+++ modules/main.js
@@ -2,15 +2,25 @@ var canvas;
 var FANS = 0;
 var FPS = 0;
 
+var shack;
+var mouseSprite;
+
 function setup(){
 	createCanvas(1000, 500);
 	textAlign(CENTER);
 
-	
+	mouseSprite = createSprite(mouseX, mouseY, 1, 1);
+	mouseSprite.visible = false;
+
+	shack = createSprite(buildingWidth(width)/2, upgradeHeight(height)/2, 50, 50);
+	shack.shapeColor = 255;
 }
 
 function draw(){
 	background(100);
+	mouseSprite.position.x = mouseX;
+	mouseSprite.position.y = mouseY;
+
 
 	line(0, upgradeHeight(height), buildingWidth(width), upgradeHeight(height));
 	line(buildingWidth(width), 0, buildingWidth(width), height);
@@ -19,6 +29,10 @@ function draw(){
 	//drawing FPS and FANS
 	text(FANS + "\n" + FPS, buildingWidth(width)/2, upgradeHeight(height)/2 * 1/4);
 
-	rect(buildingWidth(width)/2-25, upgradeHeight(height)/2-25, 50, 50);
+	if(shack.overlap(mouseSprite)){
+		alert("overlap");
+	}
 
+
+	drawSprites();
 }
